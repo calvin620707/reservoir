70aea2f24b36cef3ac545da0e6ce47cafb7cf73d
Calvin Wu <calvin620707@msn.com>
2017-05-30 19:18:05 +0800

    fix sheets templatesˆ

--- sheets/views.py
+++ sheets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 @login_required
 def add_costs(request):
     return render(request, 'sheets/add_costs.html')
