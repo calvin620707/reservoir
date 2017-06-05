d7ee908598b9b09c5074d14ed542afaa1260c90a
Calvin Wu <calvin620707@msn.com>
2017-06-05 23:00:41 +0800

    define cost record model

--- sheets/models.py
+++ sheets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class CostRecord(models.Model):
+    project = models.ForeignKey('accounts.Project', on_delete=models.CASCADE, db_index=True)
+    payer = models.ForeignKey('accounts.User', on_delete=models.CASCADE, db_index=True)
+    calculated = models.BooleanField(default=False, db_index=True)
+    name = models.CharField(max_length=30)
+    cost = models.FloatField(blank=False, null=False)
+    datetime = models.DateTimeField(default=timezone.now)
+    comment = models.TextField(blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['project', 'payer']),
+            models.Index(fields=['project', 'payer', 'calculated']),
+        ]
