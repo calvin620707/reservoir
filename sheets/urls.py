c13c2f22f2942311913bfcdd23a2f9d22c6d8081
Calvin Wu <calvin620707@msn.com>
2017-07-09 16:42:22 +0800

    refine cost template

--- sheets/urls.py
+++ sheets/urls.py
@@ -7,7 +7,7 @@ app_name = 'sheets'
 urlpatterns = [
     url(r'^costs$', login_required(views.CostListView.as_view()), name='cost-list'),
     url(r'^costs/add$', login_required(views.AddCostView.as_view()), name='add-costs'),
-    url(r'^cost/(?P<pk>\d+)$', login_required(views.CostDetailView.as_view()), name="cost-details"),
-    url(r'^cost/(?P<pk>\d+)/update$', login_required(views.CostUpdateView.as_view()), name='update-cost'),
-    url(r'^cost/(?P<pk>\d+)/delete$', login_required(views.CostDeleteView.as_view()), name='delete-cost')
+    url(r'^costs/(?P<pk>\d+)$', login_required(views.CostDetailView.as_view()), name="cost-details"),
+    url(r'^costs/(?P<pk>\d+)/update$', login_required(views.CostUpdateView.as_view()), name='update-cost'),
+    url(r'^costs/(?P<pk>\d+)/delete$', login_required(views.CostDeleteView.as_view()), name='delete-cost')
 ]
