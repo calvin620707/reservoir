539910366fa2bfd3e9803b8c9c5b382d656268ad
Calvin Wu <calvin620707@msn.com>
2017-07-09 17:36:07 +0800

    finish report display

--- accounts/util.py
+++ accounts/util.py
@@ -3,7 +3,7 @@ from accounts.models import ProjectMembership
 
 def refresh_project_memberships(project, new_members):
     members = new_members
-    rate = abs(100 / len(members))
+    rate = round(100 / len(members))
     ProjectMembership.objects.filter(project=project).delete()
     for m in members:
         ProjectMembership.objects.create(project=project, user=m, rate=rate)
--- reports/views.py
+++ reports/views.py
@@ -1,16 +1,38 @@
+from collections import namedtuple, defaultdict
+
 from django.shortcuts import render
 from django.views import View
 
 from sheets.models import CostRecord
 
 
+class ReportResult:
+    sum = 0
+    avt = 0.0
+    user_reports = {}
+
+
 class ProjectReportView(View):
     def get(self, request):
         proj = request.user.current_project
         costs = CostRecord.objects.filter(project=proj).filter(calculated=False)
+        result = ReportResult()
+
+        for user in proj.members.all():
+            result.user_reports[user] = {'paid': 0, 'result': {'should_get': True, 'value': 0}}
+
         for cost in costs:
-            pass
-        return render(request, 'reports/project_report.html', {'costs': costs})
+            result.sum += cost.cost
+            result.user_reports[cost.payer]['paid'] += cost.cost
+
+        result.avg = round(result.sum / proj.members.count())
+
+        for user in result.user_reports:
+            diff = result.avg - result.user_reports[user]['paid']
+            result.user_reports[user]['result']['should_get'] = diff > 0
+            result.user_reports[user]['result']['value'] = abs(diff)
+
+        return render(request, 'reports/project_report.html', {'costs': costs, 'result': result})
 
     def post(self, request):
         raise NotImplemented
