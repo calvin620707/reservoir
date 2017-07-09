c6086f4165366202642334be14366accee3c8278
Calvin Wu <calvin620707@msn.com>
2017-07-09 18:10:16 +0800

    draft finished

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,6 @@
 from collections import namedtuple, defaultdict
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from sheets.models import CostRecord
@@ -25,7 +25,7 @@ class ProjectReportView(View):
             result.sum += cost.cost
             result.user_reports[cost.payer]['paid'] += cost.cost
 
-        result.avg = round(result.sum / proj.members.count())
+        result.avg = result.sum / proj.members.count()
 
         for user in result.user_reports:
             diff = result.avg - result.user_reports[user]['paid']
@@ -35,4 +35,11 @@ class ProjectReportView(View):
         return render(request, 'reports/project_report.html', {'costs': costs, 'result': result})
 
     def post(self, request):
-        raise NotImplemented
+        costs = []
+        for cost_id in request.POST.getlist('cost_ids'):
+            cost_record = get_object_or_404(CostRecord, id=cost_id)
+            cost_record.calculated = True
+            cost_record.save()
+            costs.append(cost_record)
+
+        return render(request, 'reports/final_project_report.html', {'costs': costs})
