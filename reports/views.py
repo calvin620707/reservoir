2d19c72f651bff1ea2e1a2647b77c738237b78e4
Calvin Wu <calvin620707@msn.com>
2017-07-23 19:18:50 +0800

    refine report page

--- reports/views.py
+++ reports/views.py
@@ -8,7 +8,7 @@ from sheets.models import CostRecord
 
 class ReportResult:
     sum = 0
-    avt = 0.0
+    avg = 0.0
     user_reports = {}
 
 
@@ -16,23 +16,14 @@ class ProjectReportView(View):
     def get(self, request):
         proj = request.user.current_project
         costs = CostRecord.objects.filter(project=proj).filter(calculated=False)
-        result = ReportResult()
 
-        for user in proj.members.all():
-            result.user_reports[user] = {'paid': 0, 'result': {'should_get': True, 'value': 0}}
+        result = self._calculate_report(costs, proj)
 
-        for cost in costs:
-            result.sum += cost.cost
-            result.user_reports[cost.payer]['paid'] += cost.cost
-
-        result.avg = result.sum / proj.members.count()
-
-        for user in result.user_reports:
-            diff = result.avg - result.user_reports[user]['paid']
-            result.user_reports[user]['result']['should_get'] = diff > 0
-            result.user_reports[user]['result']['value'] = abs(diff)
-
-        return render(request, 'reports/project_report.html', {'costs': costs, 'result': result})
+        return render(
+            request,
+            'reports/project_report.html',
+            {'costs': costs, 'result': result, 'confirmed': False}
+        )
 
     def post(self, request):
         costs = []
@@ -42,4 +33,27 @@ class ProjectReportView(View):
             cost_record.save()
             costs.append(cost_record)
 
-        return render(request, 'reports/final_project_report.html', {'costs': costs})
+        proj = request.user.current_project
+        result = self._calculate_report(costs, proj)
+
+        return render(
+            request,
+            'reports/project_report.html',
+            {'costs': costs, 'result': result, 'confirmed': True}
+        )
+
+    def _calculate_report(self, costs, proj):
+        result = ReportResult()
+
+        for user in proj.members.all():
+            result.user_reports[user] = {'paid': 0, 'result': {'should_get': True, 'value': 0}}
+        for cost in costs:
+            result.sum += cost.cost
+            result.user_reports[cost.payer]['paid'] += cost.cost
+        result.avg = result.sum / proj.members.count()
+        for user in result.user_reports:
+            diff = result.avg - result.user_reports[user]['paid']
+            result.user_reports[user]['result']['should_get'] = diff < 0
+            result.user_reports[user]['result']['value'] = abs(diff)
+
+        return result
